1a782526a5fb081a358c74f89d63e071e89e96c6
SARAWIN_B <sarawin.14156@gmail.com>
2022-10-09 12:35:27 +0700

    send Data and get data

---
+++ script_POST.js
@@ -0,0 +1,31 @@
+var DataFrom = document.getElementById("send-Data");
+
+DataFrom.addEventListener("submit", function (sending) {
+  sending.preventDefault();
+  var studentid = document.getElementById("st_id");
+  var studentname = document.getElementById("st_name");
+  var senname = document.getElementById("sen_name");
+  var sentype = document.getElementById("sen_type");
+  var senunit = document.getElementById("sen_unit");
+  var senvalue = document.getElementById("sen_value");
+  fetch("https://midterm-exam-010723313-2022.herokuapp.com/student_post", {
+    method: "POST",
+    body: JSON.stringify({
+      st_id: studentid.value,
+      st_name: studentname.value,
+      sensor_name: senname.value,
+      sensor_type: sentype.value,
+      sensor_unit: senunit.value,
+      sensor_value: senvalue.value,
+    }),
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+});
--- script_leveltrans.js
+++ script_leveltrans.js
@@ -1,26 +1,26 @@
 document.addEventListener('DOMContentLoaded',function(){
-    fetch('https://midterm-exam-010723313-2022.herokuapp.com/device?sensor_type=flow-transducer')
+    fetch('https://midterm-exam-010723313-2022.herokuapp.com/device?sensor_type=level-transducer')
     .then(response=>response.json())
     .then(data => {
         // Display the value in the console F12
         console.log(data[0].value); 
         console.log(data[1].value);
         console.log(data[2].value);
-        // flow-transducer-0 Display in the HTML 
+        // level-transducer-0 Display in the HTML 
         document.querySelector('#name_0').innerHTML = `name : ${data[0].name}`;
         document.querySelector('#type_0').innerHTML = `Type : ${data[0].type}`;
         document.querySelector('#unit_0').innerHTML = `unit : ${data[0].unit}`;
         document.querySelector('#range_0').innerHTML = `range : ${data[0].range}`;
         document.querySelector('#plant-site_0').innerHTML = `plant-site : ${data[0]["plant-site"]}`;
         document.querySelector('#value_0').innerHTML = `value : ${data[0].value}`;
-        // flow-transducer-1 Display in the HTML
+        // level-transducer-1 Display in the HTML
         document.querySelector('#name_1').innerHTML = `name : ${data[1].name}`;
         document.querySelector('#type_1').innerHTML = `Type : ${data[1].type}`;
         document.querySelector('#unit_1').innerHTML = `unit : ${data[1].unit}`;
         document.querySelector('#range_1').innerHTML = `range : ${data[1].range}`;
         document.querySelector('#plant-site_1').innerHTML = `plant-site : ${data[1]["plant-site"]}`;
         document.querySelector('#value_1').innerHTML = `value : ${data[1].value}`;
-        // flow-transducer-2 Display in the HTML 
+        // level-transducer-2 Display in the HTML 
         document.querySelector('#name_2').innerHTML = `name : ${data[2].name}`;
         document.querySelector('#type_2').innerHTML = `Type : ${data[2].type}`;
         document.querySelector('#unit_2').innerHTML = `unit : ${data[2].unit}`;
